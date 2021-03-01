aa9ab5280a5aab5de54cd062c931c6d40c6a4e67
mlokman286 <mlokman286@gmail.com>
2021-03-02 00:40:01 +0600

    now i understand the difference between dobule-equal and triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,10 @@
+//double-equal can check and compare similarity between two variable value
+//one the other hand, triple-equal check full type and looking every difference of two variable-value 
+const first = 0;
+const second = false;
+if (first == second) {
+    console.log('This condition is True.')
+}
+else{
+    console.log('This condition is False');
+}
