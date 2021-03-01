f59de93bd4e44edd509e29b85d9954999b040c77
mlokman286 <mlokman286@gmail.com>
2021-03-02 01:44:06 +0600

    learned somthing about map,filter and find

---
+++ map-filter.js
@@ -0,0 +1,26 @@
+const numbers = [3, 4, 5, 6, 7, 8]
+// const output = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     const num = numbers[i];
+//     const result = num * num;
+//     output.push(result)
+// }
+
+// function squre(element) {
+//     return element * element;
+// }
+
+// const result=numbers.map(function (element) {
+//     return element*element;
+// })
+
+// const square = element => element * element;         // short method of ES-6;
+// const square = x => x * x;
+
+// const result = numbers.map(x => x * x)
+// console.log(result);
+
+const bigger = numbers.filter(x => x < 5);
+
+const isThere= numbers.find(x => x > 6);
+console.log(isThere);
--- practice.js
+++ practice.js
@@ -1,6 +1,24 @@
-var num = NaN;
-if(num){
-    console.log('This value is true')
-}else{
-    console.log('This value is false');
-}
+//map, filter, fine
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9]
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     console.log(element*element); ;
+// }
+
+// function square(element) {
+//     return element*element;
+// }
+
+// const result = numbers.map(function (element,index,array) {
+//     console.log(array)
+// })
+
+// const result = numbers.map(elements)
+
+// filter;
+// const bigger = numbers.filter(x => x<5)
+// console.log(bigger);
+
+//find
+const isThere = numbers.find(x =>x >5)
+console.log(isThere);
