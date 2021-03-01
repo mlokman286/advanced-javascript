36fd93ce9277f8b465ee11c593c8476c171004b3
mlokman286 <mlokman286@gmail.com>
2021-03-01 22:43:01 +0600

    adding true-false js file.

---
+++ true-false.js
@@ -0,0 +1,22 @@
+//Falsy value
+// 0 ---> means false
+// "" --->empty string means false;
+// undefined --->variable means false.
+// null --->means false.
+//NaN -----> means false.
+// false --- is a false value 
+
+//Truthy value;
+// " "---> string with white space is true.
+//Negative or positive number is also truthy value.
+// "0" ---> is true value.
+//[] empty arry is true.
+
+let Name = false
+console.log(Name);
+if (Name) {
+    console.log('condition is true')
+}
+else{
+    console.log('conditoin is false')
+}
